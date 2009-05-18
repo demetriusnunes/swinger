05c7abb621c56ca08445676caee3febfc59e7fcd
Demetrius Nunes <demetriusnunes@gmail.com>
2009-05-18 08:50:55 -0300

    Added menus

---
+++ features/menu.feature
@@ -0,0 +1,11 @@
+Feature: Menu locators and events
+
+  Scenario: Menu clicking
+    Given the frame "SwingSet" is visible
+      And the frame "SwingSet" is a container
+    When I click the menu "File"
+      And I click the menu "Look & Feel"
+      And I click the menu "Look & Feel/Motif Look & Feel"
+      And I click the menu "Look & Feel/Java Look & Feel"
+      And I click the menu "Themes"
+      And I click the menu "Options"
---
+++ features/support/step_definitions/menu_steps.rb
@@ -0,0 +1,20 @@
+import org.netbeans.jemmy.operators.JMenuBarOperator
+import org.netbeans.jemmy.operators.JMenuOperator
+import org.netbeans.jemmy.operators.JMenuItemOperator
+
+def menu(path)
+  check_container "Menu"
+  parts = path.split("/")
+  @menu_bar = JMenuBarOperator.new(@container)
+  @menu = JMenuOperator.new(@menu_bar, parts.first)
+  if parts.size > 1
+    @menu.push
+    @menu.show_menu_item(path, "/")
+  else
+    @menu
+  end
+end
+
+When /^I click the menu "([^\"]*)"$/ do |path|
+  menu(path).push
+end
