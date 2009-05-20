5cc5dd904c5da4e90832d2cdd825e8522d523188
Demetrius Nunes <demetriusnunes@gmail.com>
2009-05-20 18:48:33 -0300

    Fixed text field text entering and addded password field

--- features/support/step_definitions/textfield_steps.rb
+++ features/support/step_definitions/textfield_steps.rb
@@ -1,11 +1,20 @@
 import org.netbeans.jemmy.operators.JTextFieldOperator
+import org.netbeans.jemmy.operators.JPasswordFieldOperator
 
 def text_field(id, container = @container)
   JTextFieldOperator.new(container, string_or_numeric_id(id))
 end
 
+def password_field(id, container = @container)
+  JPasswordFieldOperator.new(container, string_or_numeric_id(id))
+end
+
 When t(/^I fill the field "([^\"]*)" with "([^\"]*)"$/) do |name, text|
-  text_field(name).enter_text(text)
+  text_field(name).text = text
+end
+
+When t(/^I fill the password field "([^\"]*)" with "([^\"]*)"$/) do |name, text|
+  password_field(name).text = text
 end
 
 Then /^the field "([^\"]*)" should be "([^\"]*)"$/ do |name, text|
