8639b41a261c1e4fdc116b77960cfd0283183785
demetriusnunes <demetriusnunes@gmail.com>
2009-10-02 01:17:32 -0300

    fixing list step definition when it doesn't find an item

--- lib/swinger/step_definitions/list_steps.rb
+++ lib/swinger/step_definitions/list_steps.rb
@@ -2,6 +2,7 @@ When t(/^I select "([^\"]*)" in the list "([^\"]*)"$/) do |value, id|
   value_id = string_or_numeric_id(value)
   operator = list(id)
   idx = value_id.is_a?(String) ? operator.find_item_index(value_id) : value_id
+  raise "Could not find item #{value_id} in list #{id}" if idx < 0
   operator.source.set_selected_index(idx)
 end
 
